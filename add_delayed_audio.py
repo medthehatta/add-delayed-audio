74a28339dcc3722fe4576219a6283b5a28b9083a
Med Mahmoud <mmahmoud@riverbed.com>
2018-04-29 16:09:23 -0400

    Make formatting not horrifying

--- add_delayed_audio.py
+++ add_delayed_audio.py
@@ -1,43 +1,61 @@
 #!/usr/bin/python
 
+
+from __future__ import (
+    print_function,  unicode_literals,  division,
+)
+
+
 import pygst
 pygst.require("0.10")
+
 import gst
 import pygtk
 import gtk
 import sys
 
-class Main:
+
+class Main(object):
+
     def __init__(self):
-	#this just reads the command line args
-	try:
-		DELAY = float(sys.argv[1])
-		DELAY = long(DELAY * 1000000000)
-		print DELAY 
-	except IndexError:
-		DELAY = 0
+        """Initialize."""
+
+        # This just reads the command line args
+        try:
+            delay = int(float(sys.argv[1]) * 1e9)
+        except IndexError:
+            delay = 0
+
+        print('Delay: {}'.format(delay))
 
         self.delay_pipeline = gst.Pipeline("mypipeline")
-	#ALSA
-	self.audiosrc = gst.element_factory_make("alsasrc", "audio")
-	self.audiosrc.set_property("device","default")
+
+        # ALSA
+        self.audiosrc = gst.element_factory_make("alsasrc",  "audio")
+        self.audiosrc.set_property("device", "default")
         self.delay_pipeline.add(self.audiosrc)
-	#Queue
-	self.audioqueue = gst.element_factory_make("queue","queue1")
-	self.audioqueue.set_property("max-size-time",0)
-	self.audioqueue.set_property("max-size-buffers",0)
-	self.audioqueue.set_property("max-size-bytes",0)
-	self.audioqueue.set_property("min-threshold-time",DELAY)
-	self.audioqueue.set_property("leaky","no")
-	self.delay_pipeline.add(self.audioqueue)
-	#Audio Output
-        self.sink = gst.element_factory_make("autoaudiosink", "sink")
+
+        # Queue
+        self.audioqueue = gst.element_factory_make("queue", "queue1")
+        self.audioqueue.set_property("max-size-time", 0)
+        self.audioqueue.set_property("max-size-buffers", 0)
+        self.audioqueue.set_property("max-size-bytes", 0)
+        self.audioqueue.set_property("min-threshold-time", delay)
+        self.audioqueue.set_property("leaky", "no")
+        self.delay_pipeline.add(self.audioqueue)
+
+        # Audio Output
+        self.sink = gst.element_factory_make("autoaudiosink",  "sink")
         self.delay_pipeline.add(self.sink)
-	#Link the elements
+
+        # Link the elements
         self.audiosrc.link(self.audioqueue)
-	self.audioqueue.link(self.sink)
-	#Begin Playing
+        self.audioqueue.link(self.sink)
+
+        # Begin Playing
         self.delay_pipeline.set_state(gst.STATE_PLAYING)
 
-start=Main()
-gtk.main()
+
+if __name__ == '__main__':
+    start = Main()
+    gtk.main()
