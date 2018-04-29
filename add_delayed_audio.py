4fc5d171562a8cb2855dde0ad00abd748e1c1b5f
Med Mahmoud <mmahmoud@riverbed.com>
2018-04-29 16:20:50 -0400

    Use extracted functions

--- add_delayed_audio.py
+++ add_delayed_audio.py
@@ -18,43 +18,69 @@ import gtk
 import sys
 
 
+def audio_device_source(name, device='default'):
+    """An audio source coming from a device."""
+    source = gst.element_factory_make('alsasrc', name)
+    source.set_property('device', device)
+    return source
+
+
+def queue_with_delay(name, delay=0):
+    """A queue that introduces a delay."""
+    queue = gst.element_factory_make('queue', name)
+    queue.set_property("max-size-time", 0)
+    queue.set_property("max-size-buffers", 0)
+    queue.set_property("max-size-bytes", 0)
+    queue.set_property("min-threshold-time", delay)
+    queue.set_property("leaky", "no")
+    return queue
+
+
+def audio_sink(name):
+    """An automatic audio sink."""
+    sink = gst.element_factory_make('autoaudiosink', name)
+    return sink
+
+
+def pipeline(*components):
+    """Assemble a gstreamer pipeline of components."""
+    _pipeline = gst.Pipeline('mypipeline')
+
+    for component in components:
+        _pipeline.add(component)
+
+    # If we have fewer than 2 components, we're done
+    if len(components) < 2:
+        return _pipeline
+
+    # If we have more than 1 component, link them together
+    previous_component = components[0]
+    for component in components[1:]:
+        previous_component.link(component)
+        previous_component = component
+
+    return _pipeline
+
+
 class Main(object):
     """The main Gtk+ class running the Gstreamer pipeline."""
 
     def __init__(self):
         """Initialize."""
 
-        # This just reads the command line args
+        # Read the command line args to get the delay
         try:
             delay = int(float(sys.argv[1]) * 1e9)
         except IndexError:
             delay = 0
-
         print('Delay: {}'.format(delay))
 
-        self.delay_pipeline = gst.Pipeline("mypipeline")
-
-        # ALSA
-        self.audiosrc = gst.element_factory_make("alsasrc",  "audio")
-        self.audiosrc.set_property("device", "default")
-        self.delay_pipeline.add(self.audiosrc)
-
-        # Queue
-        self.audioqueue = gst.element_factory_make("queue", "queue1")
-        self.audioqueue.set_property("max-size-time", 0)
-        self.audioqueue.set_property("max-size-buffers", 0)
-        self.audioqueue.set_property("max-size-bytes", 0)
-        self.audioqueue.set_property("min-threshold-time", delay)
-        self.audioqueue.set_property("leaky", "no")
-        self.delay_pipeline.add(self.audioqueue)
-
-        # Audio Output
-        self.sink = gst.element_factory_make("autoaudiosink",  "sink")
-        self.delay_pipeline.add(self.sink)
-
-        # Link the elements
-        self.audiosrc.link(self.audioqueue)
-        self.audioqueue.link(self.sink)
+        # Set up the pipeline
+        self.delay_pipeline = pipeline(
+            audio_device_source('default'),
+            queue_with_delay(delay),
+            audio_sink(),
+        )
 
         # Begin Playing
         self.delay_pipeline.set_state(gst.STATE_PLAYING)
